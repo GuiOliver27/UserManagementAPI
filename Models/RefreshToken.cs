30b77244d27d9c3196e126a3b867121249158692
Guilherme Monção <guilherme_oliveira27@outlook.com>
2026-03-15 18:37:47 -0400

    Feat: Implementação de User e RefreshToken

---
+++ Models/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagementAPI.Models;
+
+public class RefreshToken {
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Token { get; set; } = string.Empty;
+    public DateTime ExpiresAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsRevoked { get; set; } = false;
+    public string? ReplacedByToken { get; set; }
+
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+}

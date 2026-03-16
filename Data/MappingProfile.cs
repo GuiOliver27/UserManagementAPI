d5269bf188de9dad80fefc19af953fdedccfe75c
Guilherme Monção <guilherme_oliveira27@outlook.com>
2026-03-15 21:47:31 -0400

    Feat: Correção de bugs e implementação de MappingProfile

---
+++ Data/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UserManagementAPI.DTOs;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Data;
+
+public class MappingProfile : Profile {
+    public MappingProfile() {
+        CreateMap<User, UserResponse>();
+        CreateMap<RegisterRequest, User>()
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore());
+    }
+}
